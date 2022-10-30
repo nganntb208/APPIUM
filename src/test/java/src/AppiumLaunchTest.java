73ae04b3ba6b3ff65db81e9a989506c4b874f3f7
ngannguyen <nganntb208@gmail.com>
2022-10-30 23:28:46 +0700

    no message

---
+++ src/test/java/src/AppiumLaunchTest.java
@@ -0,0 +1,44 @@
+package src;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class AppiumLaunchTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        AppiumDriver<MobileElement> driver = null;
+
+        // Desired Caps
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("udid", "6c0a96e0");
+        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+        // Specify Appium Server URL
+        URL appiumServer = null;
+
+        try {
+            appiumServer = new URL("http://localhost:4723/wd/hub");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (appiumServer == null) {
+            throw new RuntimeException("[ERR] Somehow, we couldn't construct Appium server URL");
+        }
+
+        driver = new AndroidDriver<>(appiumServer, desiredCapabilities);
+
+        // DEBUG PURPOSE ONLY
+        Thread.sleep(3000);
+
+        driver.quit();
+
+    }
+}
